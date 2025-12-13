fffe07ad9f5f6c7cfa6af041a366f5eee8969d1b
imransiam <imransiam74@gmail.com>
2025-12-13 13:47:55 +0600

    fetched all data

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-const port = 3000;
+const port = 5000;
 
 
 const app = express();
 app.use(cors());
-
+app.use(express.json());
 
 
 
@@ -26,6 +26,49 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db('petservices');
+    const servicesCollection = database.collection('services');
+
+// post or save services to database
+    app.post('/services', async (req, res) => {
+  const data = req.body;
+  const date = new Date();
+  data.CreatedAt = date;
+  console.log("Received:", data);
+  const result = await servicesCollection.insertOne(data);
+  res.status(200).send(result);
+});
+// get or fetch services from database
+
+    app.get('/services', async (req, res) => {
+  const { category } = req.query;  // optional query param
+  let query = {};
+
+  if (category) {
+    query = { category };  // filter by category
+  }
+
+  try {
+    const result = await servicesCollection.find(query).toArray();
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: "Server error" });
+  }
+});
+
+
+    app.get('/services/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.send(service);
+    })
+
+    
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
