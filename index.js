2969e145ba8dfe6e8f97e2951b60264ac30a06bc
imransiam <imransiam74@gmail.com>
2025-12-16 01:17:57 +0600

    delete function completed

--- index.js
+++ index.js
@@ -91,6 +91,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/deleteListing/:id', async(req,res)=>{
+      const id =req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await servicesCollection.deleteOne(query);
+      res.send(result);
+    })
+
     
 
 
